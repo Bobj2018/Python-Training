f1bd63632c8afd0320af27a7ae9cd594f6c4e928
Joshua Rieth <joshua.m.rieth@gmail.com>
2020-03-18 23:20:01 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,38 @@
+# Numbers
+
+print(5 + 6)
+print("5" + "6")
+print(5.23 * 6.1)
+print(int("5") + int("22"))
+
+# Strings
+
+print('Hello World')
+print("Don't do that!")
+print('She said "Don\'t do that"')
+
+# Basic String Manipulation
+
+print("Hello, " + "Joshua")
+print("This costs $" + str(6) + " bucks")
+print("Hello:Joshua".split(":"))
+print("Hello, " + "Hello:Joshua:World".split(":")[1])
+
+# Lists
+print("I like " + str(["Apples", "Oranges", "Limes", 15][3]))
+
+# Dictionaries
+print("My name is " + {"name":"Joshua", "age": 20}["name"])
+
+# Variables
+greeting = "Hello World"
+print(greeting)
+greeting = greeting.split(" ")[0]
+print(greeting + " Joshua")
+
+# Built-in Function
+print(sorted([31,2,43,24,52,52]))
+print(sorted(["A", "a", "%"]))
+
+# Custom Fuctions
+
